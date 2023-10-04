eaf453f2d7d8f0cd147282d47e3c5a29bb88b411
GuiOP11 <143406425+GuiOP11@users.noreply.github.com>
2023-10-04 14:19:51 +0000

    2.1

--- myscript.js
+++ myscript.js
@@ -1,37 +1,4 @@
 
-function calcularSoma() {
-const num1 = parseFloat(document.getElementById('num1').value);
-const num2 = parseFloat(document.getElementById('num2').value);
-const resultado = num1 + num2;
-document.getElementById('resultado').innerText = resultado;
-
-const num3 = parseFloat(document.getElementById('num3').value);
-const num4 = parseFloat(document.getElementById('num4').value);
-const total = num3 + num4;
-document.getElementById('total').innerText = total;
-
-const num5 = parseFloat(document.getElementById('num5').value);
-const num6 = parseFloat(document.getElementById('num6').value);
-const resul = num5 + num6;
-document.getElementById('resul').innerText = resul;
-}
-
-function calcularSubtracao() {
-const num1 = parseFloat(document.getElementById('num1').value);
-const num2 = parseFloat(document.getElementById('num2').value);
-const resultado = num1 - num2;
-document.getElementById('resultado').innerText = resultado;
-
-const num3 = parseFloat(document.getElementById('num3').value);
-const num4 = parseFloat(document.getElementById('num4').value);
-const total = num3 - num4;
-document.getElementById('total').innerText = total;
-
-const num5 = parseFloat(document.getElementById('num5').value);
-const num6 = parseFloat(document.getElementById('num6').value);
-const resul = num5 - num6;
-document.getElementById('resul').innerText = resul;
-}
 function calcularMultiplicacao() {
 const num1 = parseFloat(document.getElementById('num1').value) || 0;
 const num2 = parseFloat(document.getElementById('num2').value) || 0;
@@ -40,14 +7,33 @@ document.getElementById('resultado').innerText = resultado;
 
 const num3 = parseFloat(document.getElementById('num3').value);
 const num4 = parseFloat(document.getElementById('num4').value);
-const total = num3 * num4;
-document.getElementById('total').innerText = total;
+const total1 = num3 * num4;
+document.getElementById('total1').innerText = total1;
 
 const num5 = parseFloat(document.getElementById('num5').value);
 const num6 = parseFloat(document.getElementById('num6').value);
 const resul = num5 * num6;
 document.getElementById('resul').innerText = resul;
-}
+ }//pular
+
+ function calcular2() {
+    const num7 = parseFloat(document.getElementById('num7').value) || 0;
+const num8 = parseFloat(document.getElementById('num8').value) || 0;
+const resultado1 = num7 * num8;
+document.getElementById('resultado1').innerText = resultado1;
+
+const num9 = parseFloat(document.getElementById('num9').value);
+const num10 = parseFloat(document.getElementById('num10').value);
+const total2 = num9 * num10;
+document.getElementById('total2').innerText = total2;
+
+const num11 = parseFloat(document.getElementById('num11').value);
+const num12 = parseFloat(document.getElementById('num12').value);
+const resul1 = num11 * num12;
+document.getElementById('resul1').innerText = resul1;
+
+ }
+ //pular
 
 var total =localStorage.getItem('inscritos');
 if(total !== null){
